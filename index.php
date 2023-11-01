6cc74794f2a0cbd21def4cc272090846463e4093
DylanLaptop <dylannicolet@hotmail.com>
2023-11-01 17:14:12 +0400

    Initial Setup

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Entertainement Web App</title>
+        <link rel="stylesheet" href="sass/App.css">
+    </head>
+    <body class="login">
+        <section class="logo-container">
+            <img src="assets/logo.svg" alt="Logo">
+        </section>
+        <form action="" class="form-login">
+            <h1>Login</h1>
+
+            <fieldset>
+                <input class="" type="email" placeholder="Email adress">
+                <input type="password" placeholder="Password">
+
+                <button type="submit">Login to your account</button>
+            </fieldset>
+
+            <section class="alternative-link-wrapper">
+                <p>Don't have an account</p>
+                <a href="#">Sign Up</a>
+            </section>
+        </form>
+    </body>
+</html>
